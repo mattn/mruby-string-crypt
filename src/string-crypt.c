f832adc16e0b1c2864a05c67959c3329e84488f5
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-12-11 14:28:10 +0900

    cosmetic change

--- src/string-crypt.c
+++ src/string-crypt.c
@@ -7,14 +7,13 @@ static mrb_value
 mrb_string_crypt(mrb_state* mrb, mrb_value self) {
   extern char *crypt(const char *, const char *);
   mrb_value str = mrb_nil_value();
-  char *res;
   static pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 
   mrb_get_args(mrb, "S", &str);
 
   if (pthread_mutex_lock(&m) == 0) {
     mrb_value ret = mrb_nil_value();
-    res = crypt(mrb_str_to_cstr(mrb, self), mrb_str_to_cstr(mrb, str));
+    char *res = crypt(mrb_str_to_cstr(mrb, self), mrb_str_to_cstr(mrb, str));
     if (res) ret = mrb_str_new_cstr(mrb, res);
     pthread_mutex_unlock(&m);
     return ret;
