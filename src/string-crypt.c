0ea18d806449194ab645c93241bd89d384e27fa3
Takeshi Watanabe <takechi101010@gmail.com>
2018-03-25 09:39:21 +0900

    Remove unused variable.

--- src/string-crypt.c
+++ src/string-crypt.c
@@ -25,8 +25,6 @@ mrb_string_crypt(mrb_state* mrb, mrb_value self) {
 
 void
 mrb_mruby_string_crypt_gem_init(mrb_state* mrb) {
-  struct RClass *clazz;
-
   mrb_define_module_function(mrb, mrb->string_class, "crypt", mrb_string_crypt, MRB_ARGS_REQ(1));
 }
 
